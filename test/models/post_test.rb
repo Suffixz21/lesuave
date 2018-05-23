e496dc1052f698c2bddc4a5178e184a14cce87e0
Suffixz21 <felixsean@ymail.com>
2018-05-24 00:32:25 +0200

    initial test setup

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "post likes" do
+    assert_equal 100, posts(:two).like
+  end
+
+  test "post title" do
+    assert_equal "Hundred favs on this post", posts(:two).title
+  end
+
+  test "post text" do
+    assert_equal "This post was liked by hundred users", posts(:two).text
+  end
 end
