5565b70a45bc9f8ec25533e9177e2b3e837245dc
Suffixz21 <felixsean@ymail.com>
2019-02-21 13:10:29 +0200

    add user functionality

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,31 @@
+class User < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, except: [:index, :show]
+
+    def index
+    @user = User
+    end
+
+    def show
+    @user = User.find(user_params)
+    end
+
+    def edit
+    @user = User.find(params[:id])
+    end
+
+    def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    end
+
+    private
+
+    def find_user
+    @user = User.find(params[:id])
+    end
+
+    def user_params
+    params.require(:user).permit(:id,:name, :summary, :birthday, :user_id)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   enum role: [:admin, :app_user, :designer, :banned, :editor, :photographer, :guest]
+  has_one :user
+
+  after_create :build_user
+
+  def build_user
+    User.create(user: self)
+  end
 end
