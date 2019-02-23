17317c2e9cf56a401b79521d007a9f8a26f63e5e
Suffixz21 <felixsean@ymail.com>
2019-02-23 11:19:03 +0200

    rename user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class User < ApplicationController
+class Users < ApplicationController
     before_action :find_user, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!, except: [:index, :show]
 
